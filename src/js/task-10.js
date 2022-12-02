8db227b826c16768a6ade1562f58e6c2802dde62
SashaIlchenko <sandra.ilchenko11@gmail.com>
2022-12-02 17:06:00 +0200

    done 10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,36 @@
+const refs = {
+  input: document.querySelector('#controls input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxEl: document.querySelector('#boxes'),
+}
+refs.createBtn.addEventListener('click', onAmount);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+console.dir(refs.input);
+
+function onAmount() {
+  const amount = refs.input.value;
+  createBoxes(amount);
+}
+function createBoxes(amount) {
+  let startSize = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i <= amount; i += 1) {
+    const size = startSize + i * 10;
+    const divEl = document.createElement('div');
+    divEl.style.width = `${size}px`;
+    divEl.style.height = `${size}px`;
+    divEl.style.backgroundColor = getRandomHexColor(createBoxes);
+    fragment.appendChild(divEl);
+  }
+  refs.boxEl.appendChild(fragment);
+
+}
+
+function destroyBoxes() {
+  refs.boxEl.innerHTML = "";
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
