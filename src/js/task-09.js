76fd3668d512377babc8102dd7c17f07d4360c6b
SashaIlchenko <sandra.ilchenko11@gmail.com>
2022-12-02 14:24:16 +0200

    dane 9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  body: document.querySelector('body'),
+  button: document.querySelector('.change-color'),
+  colorNameLabel: document.querySelector('.color'),
+}
+refs.button.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+
+  refs.body.style.backgroundColor = getRandomHexColor(onBtnClick);
+  refs.colorNameLabel.textContent = refs.body.style.backgroundColor;
+
+}
+
+
+
