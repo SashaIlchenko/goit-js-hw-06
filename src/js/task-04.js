dcc6cb678fba107a162f07ab63094ab71d6652dc
SashaIlchenko <sandra.ilchenko11@gmail.com>
2022-12-02 15:33:44 +0200

    done 4 task

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,17 +1,18 @@
-const counterEl = document.querySelectorAll('#counter');
 const valueEl = document.querySelector('#value');
 const btnDecrement = document.querySelector('[data-action="decrement"]');
 const btnIncrement = document.querySelector('[data-action="increment"]');
-console.log(counterEl, valueEl, btnDecrement);
-let counterValue = Number(valueEl.textContent);
-console.log(counterValue)
+
+let counterValue = 0;
+
 btnDecrement.addEventListener('click', OnBtnDecClick);
 btnIncrement.addEventListener('click', OnBtnIncClick);
-function OnBtnDecClick(event) {
-    console.dir(event);
-}
 
-function OnBtnIncClick(event) {
-    console.dir(event);
+function OnBtnDecClick() {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
+function OnBtnIncClick() {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
 }
 
